643479718348d784c3e81231c030a2971792b2de
yerlirock <yerlirock@yahoo.com>
2016-12-19 22:09:02 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,12 +1,16 @@
 # Release name
 PRODUCT_RELEASE_NAME := t0lte
 
+# Inherit device configuration
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/t0lte/t0lte.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
